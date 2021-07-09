78a226f0090a4237f2ca7bb5294afc369468eaee
Pawel Nackiewicz <it.pnackiewicz@gmail.com>
2021-07-09 19:34:24 +0200

    :wrench: add routing and navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FormComponent } from './components/form/form.component';
+import { ListComponent } from './components/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'list', component: ListComponent },
+  { path: 'form', component: FormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
